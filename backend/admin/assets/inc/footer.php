933cb40ecc6986c9708e7c8d89a1318bcbb54f4b
gk-mokaya <kevinmokaya001@gmail.com>
2024-11-07 09:10:09 +0300

    initial commit

---
+++ backend/admin/assets/inc/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                2022 - <?php echo date('Y'); ?> &copy; Developed By Mokaya Kevin :- Contact: 0790886755/0797252133 E-mail: mcpymokaya@gmail.com</a>
+            </div>
+
+        </div>
+    </div>
+</footer>
---
+++ backend/doc/assets/inc/footer1.php
@@ -0,0 +1,3 @@
+<footer class="footer footer-alt">
+            2020 - <?php echo date ('Y');?> &copy; Developed By Mokaya Kevin :- Contact: 0790886755/0797252133 E-mail: mcpymokaya@gmail.com</a> 
+</footer>
